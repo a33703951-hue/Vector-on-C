1eefb66139248d34409a883638962e547a8e1517
a33703951-hue <a33703951@gmail.com>
2025-11-25 08:12:05 +0500

    Update vector.c

--- src/vector.c
+++ src/vector.c
@@ -1,5 +1,5 @@
-#include <cstdlib>
-#include <cstring>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct{
     void* __data;
@@ -9,6 +9,7 @@ typedef struct{
 } Vector;
 
 #define __VECTOR_DEFAULT_CAPACITY 5
+#define __STRING_DEFAULT_CAPACITY 10
 
 __declspec(dllexport) Vector* CreateVector(int ellement_size){
     Vector* v=(Vector*)malloc(sizeof(Vector));
@@ -60,4 +61,58 @@ __declspec(dllexport) void* Vector_Get(Vector* v,int index){
 
 __declspec(dllexport) void Vector_Free(Vector *v){
     free(v->__data);
+    free(v);
+}
+
+
+typedef struct string{
+    char* __data;
+    int __size;
+    int __curr;
+} string;
+
+__declspec(dllexport) string* CreateString(){
+    string* obj=(string*)malloc(sizeof(string));
+    obj->__data=(char*)malloc(__STRING_DEFAULT_CAPACITY);
+    obj->__size=__STRING_DEFAULT_CAPACITY;
+    obj->__curr=0;
+    obj->__data[obj->__curr]='\0';
+    return obj;
+}
+
+__declspec(dllexport) const char* String_cstr(string* obj){
+    return obj->__data;
+}
+
+__declspec(dllexport) void String_Free(string* obj){
+    free(obj->__data);
+    free(obj);
+}
+
+__declspec(dllexport) void __String_resize(string* s,int size){
+    char* data=(char*)malloc(size);
+    memcpy(data,s->__data,s->__curr);
+    free(s->__data);
+    s->__data=data;
+    s->__size=size;
+}
+
+__declspec(dllexport) void String_addc(string* obj,const char* s){
+    if (obj->__curr+strlen(s)+1<obj->__size)
+        __String_resize(obj,obj->__size+(strlen(s)*2));
+    memcpy(obj->__data+obj->__curr,s,strlen(s));
+    obj->__curr+=strlen(s);
+    obj->__data[obj->__curr] = '\0';
+}
+
+__declspec(dllexport) void String_adds(string* obj,string* other){
+    if (obj->__curr+other->__curr+1<obj->__size)
+        __String_resize(obj,other->__size*2);
+    memcpy(obj->__data+obj->__curr,other->__data,other->__curr);
+    obj->__curr+=other->__curr;
+    obj->__data[obj->__curr] = '\0';
+}
+
+int String_size(string* o){
+    return o->__curr;
 }
